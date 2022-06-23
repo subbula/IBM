f96b09b3afaac5e41b5d82347d2e426b591649da
SubbulakshmiR <subbulakshmi.R@ibm.com>
2022-06-23 14:04:02 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+/** Hovig Ohannessian <hovigg@hotmail.com> **/
+// require('dotenv').load('./config/.env');
+const MQservice = require('./services/MQservice.js');
+var bodyParser = require('body-parser');
+var express = require('express');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.set('port', 5000);
+
+app.post('/msg',async(req, res, next)=>{
+  let { queueName, payload } = req.body;
+  await MQservice.publishToQueue(queueName, payload);
+  res.statusCode = 200;
+  res.data = {"message-sent":true};
+  res.send("ok");
+  // next();
+})
+
+app.get('/subscribe', async(req, res)=> {
+  let { queueName } = req;
+  await subscribefrmQueue(queueName, payload);
+  res.statusCode = 200;
+  res.data = {"message-sent":true};
+  // next();
+  res.end();
+});
+
+
+
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
+
+
---
+++ services/MQservice.js
@@ -0,0 +1,34 @@
+var amqp2 = require('amqplib');
+
+var amqp = require('amqplib/callback_api');
+
+const CONN_URL = "amqp://default_user_q6ES30Clry-6X3V7Lwy:kRULI4o-cuwM86NUwfo8_jFPkDO1cI4G@localhost:5671";
+
+let ch = null;
+amqp.connect(CONN_URL, function (err, conn) {
+	if (err) {
+		console.log("=========Error in queue connection==")
+		console.log(err)
+		// return callback(err)
+	}
+   conn.createChannel(function (err, channel) {
+      ch = channel;
+   });
+});
+
+exports.publishToQueue = async (queueName, data,callback) => {
+	ch.sendToQueue(queueName,  Buffer.from(data),{persistent: true});
+	// callback(null,{"200":"ok"})
+ }
+
+exports.subscribefrmQueue = async(queueName,callback)=>{
+	ch.consume(queueName, function (msg) {
+		console.log('.....');
+		callback(null,msg)
+		},{ noAck: true }
+	  );
+} 
+ process.on('exit', (code) => {
+	ch.close();
+	console.log(`Closing rabbitmq channel`);
+ });
